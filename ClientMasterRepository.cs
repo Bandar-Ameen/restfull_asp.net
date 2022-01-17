8b768287760202e4892aa8c717448f9340f2e015
BandarALMohammedi <bandarr.ameen@gmail.com>
2022-01-17 06:05:07 +0300

    Add files via upload

---
+++ ClientMasterRepository.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace AstoolTechRestFullAPI.Models
+{
+    public class ClientMasterRepository : IDisposable
+    {
+        // SECURITY_DBEntities it is your context class
+        Models.orderapiEntities1 context = new Models.orderapiEntities1();
+        //This method is used to check and validate the Client credentials
+      
+        //This method is used to check and validate the Client credentials
+        public ClientMaster__Rf ValidateClient(string ClientID, string ClientSecret)
+        {
+            return context.ClientMaster__Rf.FirstOrDefault(user =>
+             user.ClientID == ClientID
+            && user.ClientSecret == ClientSecret);
+        }
+
+        public ClientMaster__Rf ValidateClient( string Clientname)
+        {
+            return context.ClientMaster__Rf.FirstOrDefault(user =>
+             
+             user.ClientID == Clientname);
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
---
+++ UserMasterRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AstoolTechRestFullAPI.Models
+{
+    public class UserMasterRepository : IDisposable
+    {
+        // SECURITY_DBEntities it is your context class
+        Models.orderapiEntities1 context = new Models.orderapiEntities1();
+        //This method is used to check and validate the user credentials
+        public UserMaster ValidateUser(string username, string password)
+        {
+            return context.UserMaster.FirstOrDefault(user =>
+            user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && user.UserPassword == password);
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
